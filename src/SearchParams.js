6722dcf15c2378955b4a44d467347018ba0566d9
Alana Frank <alanamarielle@gmail.com>
2021-01-08 14:02:40 -0500

    commit point

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import { ANIMALS } from '@frontendmasters/pet'
+import {useDropdown} from './useDropdown'
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
-     
+  const [breeds, setBreeds] = useState([])
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds)
+  
   return (
       <div className="search-params" >
-          <form action="">
+          <form>
               <label htmlFor="location">
                   Location
                   <input 
@@ -15,9 +19,11 @@ const SearchParams = () => {
                   placeholder="Location" 
                   onChange={ e => setLocation(e.target.value)  } />
               </label>
+             <AnimalDropdown />
+             <BreedDropdown />
               <button>Submit</button>
           </form>
       </div>
-  )
+  );
 };
 export default SearchParams;
--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
 
 const App = () => {
-
   return (
     <div>
       <h1 id="something-important" >Adopt Me!</h1>
