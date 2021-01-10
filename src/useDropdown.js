d45966c0e1a1fbfc38fd3a2fb16198088e4c82a2
Alana Frank <alanamarielle@gmail.com>
2021-01-10 12:30:08 -0500

    added reach router

--- src/useDropdown.js
+++ src/useDropdown.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 const useDropdown = (label, defaultState, options) => {
   const [state, setState] = useState(defaultState);
-  const id = `use-dropdown=${label.replace(" ", "").toLowerCase()}`;
+  const id = `use-dropdown-${label.replace(" ", "").toLowerCase()}`;
   const Dropdown = () => (
       <label htmlFor={id}>
           {label}
@@ -12,13 +12,14 @@ const useDropdown = (label, defaultState, options) => {
           onChange={e => setState(e.target.value)}
           onBlur = {e => setState(e.target.value)}
           disabled={!options.length}
-          ></select>
-          <option>All</option>
+          > 
+          <option />
           {options.map(item => (
               <option key={item} value={item}>
                   {item}
                   </option>
           ))}
+          </select>
       </label>
   );
   return [state, Dropdown, setState];
